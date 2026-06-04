e91382cd46cc8bc66e4f1fcbafc11c95b14fd09c
itschurry <cheolhee75@icloud.com>
2026-06-04 13:56:40 +0900

    refactor: 미사용 검출 및 전송 백엔드 제거

--- libs/vision-detection/include/catcheye/detection/detector_factory.hpp
+++ libs/vision-detection/include/catcheye/detection/detector_factory.hpp
@@ -5,30 +5,21 @@
 
 #include "catcheye/detection/detector.hpp"
 #include "catcheye/detection/hailo/hailo_detector.hpp"
-#include "catcheye/detection/ncnn/ncnn_detector.hpp"
 
 namespace catcheye::detection {
 
 enum class DetectorBackend {
-    Ncnn,
     Hailo,
 };
 
 struct DetectorFactoryConfig {
-    DetectorBackend backend = DetectorBackend::Ncnn;
-    NcnnDetectorConfig ncnn;
+    DetectorBackend backend = DetectorBackend::Hailo;
     HailoDetectorConfig hailo;
 };
 
 inline std::unique_ptr<IDetector> create_detector(const DetectorFactoryConfig& cfg)
 {
     switch (cfg.backend) {
-        case DetectorBackend::Ncnn:
-#if defined(CATCHEYE_VISION_DETECTION_HAS_NCNN)
-            return std::make_unique<NcnnDetector>(cfg.ncnn);
-#else
-            throw std::runtime_error("NCNN detector backend was not built");
-#endif
         case DetectorBackend::Hailo:
 #if defined(CATCHEYE_VISION_DETECTION_HAS_HAILO)
             return std::make_unique<HailoDetector>(cfg.hailo);
